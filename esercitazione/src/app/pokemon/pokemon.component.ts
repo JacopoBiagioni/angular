33e0d85c2c176916dea6939f3e871812b0dcc281
JacopoBiagioni <biagioni.jacopo@iisgalvanimi.edu.it>
2023-11-08 12:58:26 +0000

    pokemon comp

--- esercitazione/src/app/pokemon/pokemon.component.ts
+++ esercitazione/src/app/pokemon/pokemon.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon',
   templateUrl: './pokemon.component.html',
   styleUrls: ['./pokemon.component.css']
 })
-export class PokemonComponent {
+export class PokemonComponent implements OnInit{
+
+  routeObs: Observable<ParamMap> = undefined!;
+  pokemon : any;
+  pokemonServiceObs: any;
+
+
+  constructor(
+    private route: ActivatedRoute,
+    private service: PokemonService
+  ) { }
+
+
+  ngOnInit(): void {
+    this.routeObs = this.route.paramMap;
+    this.routeObs.subscribe(this.getRouterParam);
+  }
+
+  getRouterParam = (params: ParamMap) =>
+  {
+    let pokemonid = params.get('id');
+    console.log(pokemonid)
+    if (pokemonid != null)
+    {
+      this.pokemonServiceObs = this.service.getTrack(pokemonId) ;
+    this.pokemonServiceObs.subscribe((data: any)=>this.pokemon = data)
+    }
+  }
+  
+
 
 }
